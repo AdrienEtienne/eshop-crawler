638c9c9dbc6f8969d94da6debcdebd85041108e9
Adrien ETIENNE <ae@stronghold.co>
2019-09-13 08:31:24 +1200

    Refactor services/controllers

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { SuccessBody } from './dtos';
+import { AppService } from '../services/app.service';
+import { SuccessBody } from '../dtos';
 
 @Controller()
 export class AppController {
