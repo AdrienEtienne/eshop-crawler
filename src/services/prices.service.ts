f0337d0ec7f8d24b0a81f3e04ff5ab2d939c5a59
Adrien Etienne <a.etienne92@gmail.com>
2019-09-28 08:26:25 +1200

    Improv fetch price sync

--- src/services/prices.service.ts
+++ src/services/prices.service.ts
@@ -7,7 +7,8 @@ import { logger } from '../config';
 import { Price } from '../db/entities/price.entity';
 import { Game } from '../db/entities/game.entity';
 import { Region } from 'nintendo-switch-eshop';
-import { toNumber, groupBy } from 'lodash';
+import { toNumber, groupBy, Dictionary } from 'lodash';
+import * as async from 'async';
 
 @Injectable()
 export class PricesService {
@@ -28,126 +29,136 @@ export class PricesService {
     const shops = await this.shops.find();
     const games = await this.games.find({
       order: {
-        updatedAt: 'ASC',
+        updatedAt: 'DESC',
       },
     });
 
     logger.log(`Sync Prices for ${games.length} games`);
     let index = 1;
 
-    for (const game of games) {
-      const tmpRegions: Array<{
-        gameId: string;
-        region: nintendo.Region;
-      }> = [];
-      if (game.euId) {
-        tmpRegions.push({
-          gameId: game.euId,
-          region: Region.EUROPE,
-        });
-      }
-      if (game.americaId) {
-        tmpRegions.push({
-          gameId: game.americaId,
-          region: Region.AMERICAS,
-        });
-      }
-
-      const lstToCheck: Array<{
-        gameId: string;
-        shops: Shop[];
-      }> = [];
-
-      tmpRegions.forEach(tmpRegion => {
-        const gameId = tmpRegion.gameId;
-
-        const currencies = groupBy(
-          shops.filter(shop => shop.region === tmpRegion.region),
-          el => el.currency,
-        );
-
-        // tslint:disable-next-line: forin
-        for (const currency in currencies) {
-          const tmpShops = currencies[currency];
-
-          lstToCheck.push({
-            gameId,
-            shops: tmpShops,
+    const q = async.queue(
+      async (game: Game, callback: () => void) => {
+        // List regions with game id available
+        const idRegions: Array<{
+          gameId: string;
+          region: nintendo.Region;
+        }> = [];
+        if (game.euId) {
+          idRegions.push({
+            gameId: game.euId,
+            region: Region.EUROPE,
           });
         }
-      });
-
-      for (const toCheck of lstToCheck) {
-        const gameId = toCheck.gameId;
-        const gameShops = toCheck.shops;
-        let titleData: nintendo.TitleData;
-
-        for (const gameShop of gameShops) {
-          try {
-            const priceResponse = await nintendo.getPrices(
-              gameShop.code,
-              gameId,
-            );
-            if (
-              !priceResponse.prices ||
-              priceResponse.prices.length === 0 ||
-              !priceResponse.prices[0].regular_price
-            ) {
-              continue;
-            }
-            titleData = priceResponse.prices[0];
-            break;
-          } catch (error) {
-            logger.error(
-              `Cant find price for "${game.title}", country="${gameShop.country}": ${error.message}`,
-            );
-            break;
-          }
+        if (game.americaId) {
+          idRegions.push({
+            gameId: game.americaId,
+            region: Region.AMERICAS,
+          });
         }
 
-        if (!titleData) {
-          logger.warn(
-            `Cant find price for "${game.title}" shop="${gameShops[0].country}"`,
+        // List shops to check per region
+        const idShops: Array<{
+          gameId: string;
+          shops: Shop[];
+        }> = [];
+        idRegions.forEach(idRegion => {
+          const gameId = idRegion.gameId;
+
+          const currencies = groupBy(
+            shops.filter(shop => shop.region === idRegion.region),
+            el => el.currency,
           );
-          break;
-        }
 
-        for (const gameShop of gameShops) {
-          try {
-            let price = await this.prices.findOne({
-              where: { game, shop: gameShop },
+          // tslint:disable-next-line: forin
+          for (const currency in currencies) {
+            const currencyShops = currencies[currency];
+
+            idShops.push({
+              gameId,
+              shops: currencyShops,
             });
-            if (!price) {
-              price = Price.create({ game, shop: gameShop });
-            }
-            price.amount = titleData.regular_price.amount;
-            price.amountValue = toNumber(titleData.regular_price.raw_value);
-            price.currency = titleData.regular_price.currency;
-            if (titleData.discount_price) {
-              price.discountAmount = titleData.discount_price.amount;
-              price.discountAmountValue = toNumber(
-                titleData.discount_price.raw_value,
+          }
+        });
+
+        for (const toCheck of idShops) {
+          const gameId = toCheck.gameId;
+          const gameShops = toCheck.shops;
+          let titleData: nintendo.TitleData;
+
+          for (const gameShop of gameShops) {
+            try {
+              const priceResponse = await nintendo.getPrices(
+                gameShop.code,
+                gameId,
               );
-              price.onSale = true;
-            } else {
-              price.discountAmount = null;
-              price.discountAmountValue = null;
-              price.onSale = false;
+              if (
+                !priceResponse.prices ||
+                priceResponse.prices.length === 0 ||
+                !priceResponse.prices[0].regular_price
+              ) {
+                continue;
+              }
+              titleData = priceResponse.prices[0];
+              break;
+            } catch (error) {
+              logger.error(
+                `Cant find price for "${game.title}", country="${gameShop.country}": ${error.message}`,
+              );
+              break;
             }
-            await price.save();
-          } catch (error) {
-            logger.error(
-              `Cannot sync game "${game.title}" shop=${gameShop.country} : ${error.message}`,
+          }
+
+          if (!titleData) {
+            logger.warn(
+              `Cant find price for "${game.title}" shop="${gameShops[0].country}"`,
             );
             break;
           }
-        }
-      }
 
-      logger.log(`[OK] Sync Game ${index} "${game.title}"`);
-      await game.save();
-      index++;
-    }
+          for (const gameShop of gameShops) {
+            try {
+              let price = await this.prices.findOne({
+                where: { game, shop: gameShop },
+              });
+              if (!price) {
+                price = Price.create({ game, shop: gameShop });
+              }
+              price.amount = titleData.regular_price.amount;
+              price.amountValue = toNumber(titleData.regular_price.raw_value);
+              price.currency = titleData.regular_price.currency;
+              if (titleData.discount_price) {
+                price.discountAmount = titleData.discount_price.amount;
+                price.discountAmountValue = toNumber(
+                  titleData.discount_price.raw_value,
+                );
+                price.onSale = true;
+              } else {
+                price.discountAmount = null;
+                price.discountAmountValue = null;
+                price.onSale = false;
+              }
+              await price.save();
+            } catch (error) {
+              logger.error(
+                `Cannot sync game "${game.title}" shop=${gameShop.country} : ${error.message}`,
+              );
+              break;
+            }
+          }
+        }
+
+        logger.log(`[OK] Sync Game ${index++} "${game.title}"`);
+        await game.save();
+
+        callback();
+      },
+      // Concurrent limit found is 8, put 5 for safety
+      5,
+    );
+
+    games.forEach(game => q.push(game));
+
+    await q.drain();
 
     logger.log(`[OK] Sync Prices`);
   }
